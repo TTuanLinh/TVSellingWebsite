cd8a2d38c07dbecba45e2c168e7cdd3a7b734d25
TuanLinh <trumankhoc247@gmail.com>
2025-11-09 20:58:06 +0700

    Update order form validation and error handling, remove unused definitions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,47 +4,66 @@ import { z } from 'zod';
 import prisma from './prisma';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { error } from 'console';
 
-const FormCreateSchema = z.object({
+const FormSchema = z.object({
   id: z.number(),
-  userId: z.coerce.number(),
-  total: z.coerce.number(),
-  status: z.preprocess(
-    (val) => Number(val),
-    z.union([z.literal(0), z.literal(1)])
-  ),
+  userId: z.coerce.number().gt(0, {
+    message: 'Please select a customer.',
+  }),
+  total: z.coerce.number().gt(0, {
+    message: 'Total amount must be greater than 0.',
+  }),
+  status: z.enum(['0', '1'], {
+    invalid_type_error: 'Please select a status.',
+  }).pipe(z.coerce.number()),
   createdAt: z.string(),
-});
-
-const FormUpdateSchema = z.object({
-  id: z.number(),
-  userId: z.coerce.number(),
-  total: z.coerce.number(),
-  status: z.preprocess(
-    (val) => Number(val),
-    z.union([z.literal(0), z.literal(1)])
-  ),
   updatedAt: z.string(),
 });
 
-const UpdateOrder = FormUpdateSchema.omit({ id: true, updatedAt: true });
-const CreateOrder = FormCreateSchema.omit({ id: true, createdAt: true });
+const UpdateOrder = FormSchema.omit({ id: true, createdAt: true, updatedAt: true });
+const CreateOrder = FormSchema.omit({ id: true, createdAt: true, updatedAt: true });
+
+export type State = {
+  errors?: {
+    userId?: string[];
+    total?: string[];
+    status?: string[];
+  };
+  message: string | null;
+};
  
-export async function createOrder(formData: FormData) {
-  const { userId, total, status} = CreateOrder.parse({
+export async function createOrder(prevState: State, formData: FormData) {
+  const validatedFields = CreateOrder.safeParse({
     userId: formData.get('customerId'),
     total: formData.get('total'),
     status: formData.get('status'),
   });
 
-  await prisma.order.create({
-    data: {
-      userId: userId,
-      total: total,
-      status: status,
-      createdAt: new Date().toISOString(),
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields. Please check the form again.',
     }
-  });
+  }
+
+  const { userId, total, status } = validatedFields.data;
+  
+  try {
+    await prisma.order.create({
+      data: {
+        userId: userId,
+        total: total,
+        status: status,
+        // createdAt: new Date().toISOString(),
+      }
+    });
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create order.',
+    }
+  }
+  
 
   revalidatePath('/dashboard/orders');
   redirect('/dashboard/orders');
@@ -65,7 +84,7 @@ export async function updateOrder(id: string, formData: FormData) {
       userId: userId,
       total: total,
       status: status,
-      updatedAt: new Date().toISOString(),
+      // updatedAt: new Date().toISOString(),
     }
   });
  
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,4 +1,5 @@
-import { User } from '@/generated/prisma/client';
+'use client'
+
 import Link from 'next/link';
 import {
   CheckIcon,
@@ -7,7 +8,8 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createOrder } from '@/app/lib/actions';
+import { createOrder, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 type CustomerField = {
   id: number;
@@ -15,8 +17,11 @@ type CustomerField = {
 };
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {}};
+  const [state, formAction] = useActionState(createOrder, initialState);
+
   return (
-    <form action={createOrder}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -29,6 +34,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -40,6 +46,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.userId &&
+              state.errors.userId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -54,11 +68,19 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 id="total"
                 name="total"
                 type="number"
-                step="0.01"
                 placeholder="Enter VNĐ amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="order-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div id="order-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.total &&
+                state.errors.total.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
             </div>
           </div>
         </div>
@@ -77,6 +99,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="0"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="pending"
@@ -92,6 +115,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="1"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="paid"
@@ -100,6 +124,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   Paid <CheckIcon className="h-4 w-4" />
                 </label>
               </div>
+            </div>
+            <div id="status-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.status &&
+                state.errors.status.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
             </div>
           </div>
         </fieldset>
